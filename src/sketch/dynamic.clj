c65d89d8b79b011445413278ede9332e4eebdb15
schmalz <schmalz@live.co.uk>
2021-07-20 15:33:57 +0100

    Add more colours.

--- src/sketch/dynamic.clj
+++ src/sketch/dynamic.clj
@@ -2,6 +2,24 @@
   (:require [clojure.pprint :as pretty]
             [quil.core :as quil]))
 
+(def colours {:base03 [193 100 21]
+              :base02 [192 90 26]
+              :base01 [194 25 46]
+              :base00 [195 23 51]
+              :base0 [186 13 59]
+              :base1 [180 9 63]
+              :base2 [44 11 93]
+              :base3 [44 10 99]
+
+              :yellow [45 100 71]
+              :orange [8 89 80]
+              :red [1 79 86]
+              :magenta [331 74 83]
+              :violet [237 45 77]
+              :blue [205 82 82]
+              :cyan [175 74 63]
+              :green [68 100 60]})
+
 (defn save-frame-to-disk
   ([]
    (quil/save-frame (pretty/cl-format nil
@@ -12,6 +30,10 @@
    (save-frame-to-disk)
    state))
 
+(defn- stroke
+  [colour]
+  (apply quil/stroke (flatten (conj (colours colour) (rand)))))
+
 (defn- point
   []
   [(quil/random 0 (quil/width))
@@ -19,7 +41,7 @@
 
 (defn- shade
   []
-  (quil/stroke 180 9 63 (rand)) ; base1 (grey)
+  (stroke :base03)
   (apply quil/curve
          (flatten (take 4
                         (repeatedly point)))))
